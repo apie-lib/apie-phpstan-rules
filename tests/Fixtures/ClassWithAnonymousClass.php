4adf6c7cbcaa02965cba31300beeee33c0eae380
pjordaan <pjordaan@users.noreply.github.com>
2022-10-17 04:31:44 +0000

    Update file(s) "packages/apie-phpstan-rules/." from "apie-lib/apie-lib-monorepo"

--- tests/Fixtures/ClassWithAnonymousClass.php
+++ tests/Fixtures/ClassWithAnonymousClass.php
@@ -3,8 +3,10 @@ namespace Apie\Tests\ApiePhpstanRules\Fixtures;
 
 use Apie\Core\Dto\DtoInterface;
 
-class ClassWithAnonymousClass {
-    public function create() {
+class ClassWithAnonymousClass
+{
+    public function create()
+    {
         return new class() implements DtoInterface {
         };
     }
